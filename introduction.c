842d8752b970541b002673d7fb6cb3050dbc20a5
Weh <87272164+Weh-gh@users.noreply.github.com>
2022-03-24 00:47:47 +0300

    ttt

--- introduction.c
+++ introduction.c
@@ -25,6 +25,33 @@ int main() {
 	printf("%d byte\n",sizeof(float));
 	printf("%d byte\n",sizeof(double));
 
+	// Performing arithmetic operations
+	/*
+		x + y -----> balls x and y.
+		x - y ------> Subtract x from y.
+		x * y -------> Multiply x by y.
+		x / y -------> Divides x by y.
+		
+		x % y -----> Returns the remainder of x divided by y.
+		
+		+x ------> multiplies x by plus (result does not change)
+		-x ------> Multiplies x by - (if x is negative, it makes a plus; if x is plus, it makes a minus)
+	*/
+	
+	// Aritmetik işlemler(TR)
+	/*
+		x + y -----> x ile y'yi toplar.
+		x - y ------> x'den y'yi çıkarır.
+		x * y ------> x ile y'yi çarpar.
+		x / y ------> x ile y'yi böler.
+		
+		x % y -----> x'in y ile bölümünden kalanı verir.
+		
+		+x ------> x sayısını arti ile çarpar(sonuç değişmez)
+		-x ------> x sayısını - ile çarpar(x sayısı eksi ise artı yapar, x sayısı artı ise eksi yapar.)
+	*/
+	
+	
 	
 	return 0;
 }
