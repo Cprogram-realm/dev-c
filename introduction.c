141f95f1b79553ea2aa87051de0b2b5817693be9
Weh <87272164+Weh-gh@users.noreply.github.com>
2022-03-24 00:24:23 +0300

    tt

--- introduction.c
+++ introduction.c
@@ -7,5 +7,15 @@ int main() {
 	// basic
 	printf("Hello, world!");
 	
+	// Let's define our variables
+	int a1=42;
+	char b1=66;
+	char b2='B';
+	float c1=4.2;
+	double d1=0.21;
+	
+	// Let's print our variables
+	printf("%d %c %d %f %d\n",a1,b1,b2,c1,d1);
+	
 	return 0;
 }
