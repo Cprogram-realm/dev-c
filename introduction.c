c436584e86b8e6a2ec2a6ce89cab4885cb49ab12
Weh <87272164+Weh-gh@users.noreply.github.com>
2022-03-24 00:30:37 +0300

    ttt

--- introduction.c
+++ introduction.c
@@ -5,7 +5,7 @@
 int main() {
 	
 	// basic
-	printf("Hello, world!");
+	printf("Hello, world!\n");
 	
 	// Let's define our variables
 	int a1=42;
@@ -15,7 +15,16 @@ int main() {
 	double d1=0.21;
 	
 	// Let's print our variables
-	printf("%d %c %d %f %d\n",a1,b1,b2,c1,d1);
+	printf("%d %c %d %.1f %.2f %d\n",a1,b1,b2,c1,d1);
+	
+	// Calculating bytes of variables with printf
+	printf("%d byte\n"sizeof(char));
+	printf("%d byte\n"sizeof(int));
+	printf("%d byte\n"sizeof(short int));
+	printf("%d byte\n"sizeof(long int));
+	printf("%d byte\n"sizeof(float));
+	printf("%d byte\n"sizeof(double));
+
 	
 	return 0;
 }
